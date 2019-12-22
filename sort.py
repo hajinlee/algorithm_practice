957bcdb875f4d939ae3a2caafd887d375c836f4d
Hajin Lee <hajin88@gmail.com>
2019-12-22 12:53:20 -0800

    Combine test cases / add heap sort / fix merge sort space complexity

--- sort.py
+++ sort.py
@@ -1,8 +1,11 @@
-test_1 = [10, 8, 3, 12, 1, 5, 7, 3, 15]
-test_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-test_3 = [10, 8, 6, 4, 2, 0]
-test_3 = ["cat", "dog", "apple", "go"]
+import heapq
 
+tests = [
+    [10, 8, 3, 12, 1, 5, 7, 3, 8, 15],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [10, 8, 6, 4, 2, 0],
+    ["cat", "dog", "apple", "go"]
+]
 
 ## insertion sort in-place
 def insertion_sort(array):
@@ -18,72 +21,111 @@ def swap(array, index1, index2):
     array[index2] = temp
 
 def selection_sort(array):
+    # invariant: array[0:index] is sorted, array[index:end] is not sorted yet
+    index = 0
+    while index < len(array):
+        least = None
+        least_i = None
+        # find smallest element in the unsorted part of the array
+        for i in range(index, len(array)):
+            if (least is None) or array[i] < least:
+                least = array[i]
+                least_i = i
+
+        swap(array, index, least_i)
+        index += 1
+
+def heap_sort(array):
     new = []
+    heapq.heapify(array)
     while array:
-        min_item = min(array)
+        min_item = heapq.heappop(array)
         new.append(min_item)
-        array.remove(min_item)
 
     return new
 
-def merge_sort(array):
-    if len(array) < 2:
-        return array
+# merge sort in-place the part of "array" between start and last, inclusive
+def merge_sort(array, start=0, last=None):
+    if last is None:
+        last = len(array) - 1
 
-    mid = len(array) // 2
-    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    length = last - start + 1
+    mid = start + length // 2
 
-    return merge(left, right)
+    if length < 2:
+        return
 
-def merge(array1, array2):
+    merge_sort(array, start, mid-1)
+    merge_sort(array, mid, last)
+    merge(array, start, mid-1, mid, last)
+
+# merge two sorted, adjacent sub-segments of "array" into one sorted segment
+# (uses a temporary buffer but writes the final result into "array")
+def merge(array, start1, end1, start2, end2):
+    assert start2 == end1+1 # make sure segments are touching
     new = []
-    p1 = 0
-    p2 = 0
-    while p1 < len(array1) and p2 < len(array2):
-        if array1[p1] <= array2[p2]:
-            new.append(array1[p1])
+
+    p1 = start1
+    p2 = start2
+
+    while p1 <= end1 and p2 <= end2:
+        if array[p1] <= array[p2]:
+            new.append(array[p1])
             p1 += 1
         else:
-            new.append(array2[p2])
+            new.append(array[p2])
             p2 += 1
 
-    if p2 < len(array2):
-        new += array2[p2:]
+    # finish the ends
+    while p1 <= end1:
+        new.append(array[p1])
+        p1 += 1
+    while p2 <= end2:
+        new.append(array[p2])
+        p2 += 1
 
-    if p1 < len(array1):
-        new += array1[p1:]
+    assert len(new) == (end2 - start1) + 1
+    array[start1:end2+1] = new
 
-    return new
-
+# quicksort array in-place between indexes [low, high] inclusive
 def quick_sort(array, low=0, high=None):
     if high is None:
         high = len(array) -1
-
     if low < high:
-        ploc = partition(array, low, high)
-        quick_sort(array, low, ploc-1)
-        quick_sort(array, ploc+1, high)
+        p_loc = partition(array, low, high)
+        # after this point, array between [low, high] inclusive looks like:
+        # stuff less than array[p_loc] ... array[p_loc] ... stuff greater than array[p_loc]
+        quick_sort(array, low, p_loc-1)
+        quick_sort(array, p_loc+1, high)
 
+# quicksort parition array[low:high] inclusive, returning final index of pivot
 def partition(array, low, high):
-    pivot = low
-    for i in range(low+1, high):
-        if array[i] <= array[low]:
+    pivot = low # arbitrarily pick array[low] to be the pivot element
+    p_val = array[pivot] # value of the pivot
+    for i in range(low+1, high+1):
+        if array[i] <= p_val: # compare against pivot element
+            # if smaller, swap the pivot forward by one element
             pivot += 1
             array[i], array[pivot] = array[pivot], array[i]
 
     array[pivot], array[low] = array[low], array[pivot]
     return pivot
 
-# print(insertion_sort(test_1))
-# print(insertion_sort(test_2))
+for test in tests:
+    answer = sorted(test)
+    for name, method in \
+        ('selection sort', selection_sort), \
+        ('merge sort', merge_sort), \
+        ('quick sort', quick_sort), \
+        ('heap sort', heap_sort):
+        if method is heap_sort:
+            # returns new array
+            result = method(test)
+        else:
+            # operates in-place
+            temp = test[:] # make a copy
+            method(temp)
+            result = temp
 
-# print(selection_sort(test_1))
-# print(selection_sort(test_2))
-
-# print(merge_sort(test_1))
-# print(merge_sort(test_2))
-
-# print(test_1, test_2)
-# quick_sort(test_1)
-# quick_sort(test_2)
-# print(test_1, test_2)
+        if result != answer:
+            print('ERROR!', name, test, result)
